2a5943fd758e9be21252a55fc5b83535d138c229
Iqra-Ashraf <58162035+sonia7-png@users.noreply.github.com>
2020-02-17 02:43:13 -0800

    Add files via upload

---
+++ winingsound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.AudioSource.clip;
+[RequireComponent(typeof(Rigidbody))]
+public class winingsound : MonoBehaviour {
+    public AudioClip winsound;
+    public GameObject particle;
+    private Rigidbody rb;
+    private AudioSource _audiosource;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        _audiosource = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "point")
+        {
+            Destroy(col.gameObject);
+            Instantiate(particle, col.transform.position, col.transform.rotation);
+            _audiosource.clip = winsound;
+            _audiosource.Play();
+
+        }
+    }
+}
+
