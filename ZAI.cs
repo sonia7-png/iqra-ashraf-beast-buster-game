8bd424c879ef16272db8e455584af1620fc280c5
rimshaioo <58161122+rimshaioo@users.noreply.github.com>
2020-02-27 22:00:38 -0800

    Add files via upload

---
+++ ZAI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZAI : MonoBehaviour {
+
+ public GameObject thePlayer;
+    public GameObject theEnemy;
+    public float EnemySpeed ;
+    public bool attackTrigger = false;
+    public bool isAttacking = false;
+     public AudioSource help;
+
+//Call every frame
+void Update()
+{
+      transform.LookAt(thePlayer.transform);
+        if (attackTrigger == false)
+        {
+            EnemySpeed = 0.01f;
+            theEnemy.GetComponent<Animation>().Play("Walking");
+            transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, EnemySpeed);
+        }
+       if(attackTrigger==true && isAttacking == false)
+        {
+            EnemySpeed = 0; 
+            theEnemy.GetComponent<Animation>().Play("Zombie Attack");
+            StartCoroutine(InflictDamage());
+
+        }
+}
+ void OnTriggerEnter()
+    {
+        attackTrigger = true;   help.Play();   
+    }
+    void OnTriggerExit()
+    {
+        attackTrigger = false;
+    }
+     IEnumerator InflictDamage()
+    {
+		 
+        isAttacking = true;
+      yield return new WaitForSeconds(0.1f);
+
+      globalhealth2.currentHealth -= 2;
+      Healthbarscript.internalHealth -= 2f;
+        isAttacking = false;
+    }
+}
---
+++ zombiedeath.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zom : MonoBehaviour {
+
+ public GameObject thePlayer;
+    public GameObject theEnemy;
+    public float EnemySpeed = 0.01f;
+    public bool attackTrigger = false;
+    public bool isAttacking = false;
+ 
+
+//Call every frame
+void Update()
+{
+      transform.LookAt(thePlayer.transform);
+        if (attackTrigger == false)
+        {
+            EnemySpeed = 0.01f;
+            theEnemy.GetComponent<Animation>().Play("w");
+            transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, EnemySpeed);
+        }
+       if(attackTrigger==true && isAttacking == false)
+        {
+            EnemySpeed = 0; 
+            theEnemy.GetComponent<Animation>().Play("a");
+            StartCoroutine(InflictDamage());
+
+        }
+}
+ void OnTriggerEnter()
+    {
+        attackTrigger = true;   
+    }
+    void OnTriggerExit()
+    {
+        attackTrigger = false;
+    }
+     IEnumerator InflictDamage()
+    {
+		 
+        isAttacking = true;
+      yield return new WaitForSeconds(0.1f);
+   
+     GlobalHealth.currentHealth -= 10;
+     Healthbarscript.internalHealth -= 10f;
+        isAttacking = false;
+    }
+}
