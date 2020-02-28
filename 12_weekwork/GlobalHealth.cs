295d2cfb59f08a7ce1a7a48115c50c96a7207285
rimshaioo <58161122+rimshaioo@users.noreply.github.com>
2020-02-27 22:54:59 -0800

    Add files via upload

---
+++ 12_weekwork/GlobalHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalHealth : MonoBehaviour {
+    public static int currentHealth = 500;
+    public int internalHealth;
+
+
+	// Update is called once per frame
+	void Update () {
+        internalHealth = currentHealth;
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene(4);
+        }
+        if (ScringSystem.theScore==15)
+        {
+           SceneManager.LoadScene(2);
+       }
+	}
+}
