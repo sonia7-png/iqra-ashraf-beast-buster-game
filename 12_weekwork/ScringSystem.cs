d77c71b974d84e16e3b8913640f0558fc31c5167
rimshaioo <58161122+rimshaioo@users.noreply.github.com>
2020-02-27 22:57:50 -0800

    Add files via upload

---
+++ 12_weekwork/ScringSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScringSystem : MonoBehaviour {
+    public GameObject scoreText;
+    public static  int theScore;
+    
+
+    void Update()
+    {
+
+
+        scoreText.GetComponent<Text>().text = "POWER: " + theScore;
+       
+    }
+}
