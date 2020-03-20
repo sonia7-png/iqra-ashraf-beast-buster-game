b5715a8ba479e2f20d5bf6cc3186b04cf7632ded
Iqra-Ashraf <58162035+sonia7-png@users.noreply.github.com>
2020-03-20 03:51:55 -0700

    added by Iqra Ashraf(BSCSF16M032)

---
+++ LastWeek/DouglaAI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DouglaAI : MonoBehaviour {
+
+
+ public GameObject thePlayer;
+    public GameObject theEnemy;
+    public float EnemySpeed ;
+    public bool attackTrigger = false;
+    public bool isAttacking = false;
+   
+
+//Call every frame
+void Update()
+{
+      transform.LookAt(thePlayer.transform);
+        if (attackTrigger == false)
+        {
+            EnemySpeed = 0.05f;
+            theEnemy.GetComponent<Animation>().Play("Wheelbarrow Walk");
+            transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, EnemySpeed);
+        }
+       if(attackTrigger==true && isAttacking == false)
+        {
+            EnemySpeed = 0; 
+            theEnemy.GetComponent<Animation>().Play("Boxing");
+            StartCoroutine(InflictDamage());
+
+        }
+}
+ void OnTriggerEnter()
+    {
+        attackTrigger = true;  
+    }
+    void OnTriggerExit()
+    {
+        attackTrigger = false;
+    }
+     IEnumerator InflictDamage()
+    {
+		 
+        isAttacking = true;
+      yield return new WaitForSeconds(0.1f);
+
+      globalhealth2.currentHealth -= 2;
+      Healthbarscript.internalHealth -= 2f;
+        isAttacking = false;
+    }
+}
---
+++ LastWeek/DouglaDeath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DouglaDeath : MonoBehaviour {
+ public int EnemyHealth = 20;
+    public GameObject theEnemy;
+    public int statuscheck;
+    public AudioSource Death;
+    public ParticleSystem particleBurst;
+    private bool played;
+    void DamageZombie(int DamageAmount)
+    {
+        EnemyHealth -= DamageAmount;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(EnemyHealth<=0 && statuscheck == 0)
+        {
+            this.GetComponent<DouglaAI>().enabled = false;
+            this.GetComponent<BoxCollider>().enabled = false;
+            statuscheck = 2;
+            theEnemy.GetComponent<Animation>().Stop("Wheelbarrow Walk");
+			 if (!played)
+            {
+                particleBurst.Play();
+                played = true;
+            }
+            theEnemy.GetComponent<Animation>().Play("Dying");
+           Death.Play();
+        }
+	}
+}
